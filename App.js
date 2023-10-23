25d5a85f90b76b809cd9f240acb64d2664d0a677
Krastan Dimitrov <krastan.dimitrov@spoton.com>
2023-10-23 17:09:23 -0500

    Add TextEncoding and Events polyfull

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { LoginForm } from "./LoginForm";
+import "text-encoding-polyfill";
 import { server } from "./mocks/devServer";
 import "react-native-url-polyfill/auto";
-
 export default function App() {
   server.listen();
   return <LoginForm />;
