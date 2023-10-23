65a5c0f1506b31b091ab651fb3efe2eb372f18d2
Krastan Dimitrov <krastan.dimitrov@spoton.com>
2023-10-23 16:56:25 -0500

    Update msw to 2.0

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,24 +1,23 @@
-import { rest } from "msw";
+import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  rest.post("https://example.com/login", async (req, res, ctx) => {
-    const { username } = await req.json();
-    return res(
-      ctx.json({
-        id: "f79e82e8-c34a-4dc7-a49e-9fadc0979fda",
-        username: username,
-        firstName: "John",
-        lastName: "Maverick",
-      })
-    );
+  http.post("https://example.com/login", async ({ request }) => {
+    const body = await request;
+    const { username } = await body.json();
+    return HttpResponse.json({
+      id: "f79e82e8-c34a-4dc7-a49e-9fadc0979fda",
+      username: username,
+      firstName: "John",
+      lastName: "Maverick",
+    });
   }),
-  rest.get("https://example.com/cookie", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ message: "Jimbo34" }));
+  http.get("https://example.com/cookie", () => {
+    return HttpResponse.json({ message: "Jimbo34" }, { status: 200 });
   }),
-  rest.post("http://192.168.0.105:8081/symbolicate", (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.post("http://192.168.0.105:8081/symbolicate", () => {
+    return HttpResponse.json(null, { status: 200 });
   }),
-  rest.post("http://192.168.0.105:8081/logs", (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.post("http://192.168.0.105:8081/logs", () => {
+    return HttpResponse.json(null, { status: 200 });
   }),
 ];
